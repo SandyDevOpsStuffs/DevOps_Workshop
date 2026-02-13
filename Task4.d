c868d4134117b7a54c1184c2bbc125f6e84a5465
SandyDevOpsStuffs <147721575+SandyDevOpsStuffs@users.noreply.github.com>
2026-02-14 02:42:41 +0530

    Create Task4.d

---
+++ Task4.d
@@ -0,0 +1,6 @@
+# **Task #4**
+# **Automate EC2 Instance Creation using Terraform and Install Apache Web Server on the Instances using Ansible**
+
+Please refer my blog:
+
+https://medium.com/@sandy.devops.stuffs/automate-ec2-instance-creation-using-terraform-and-install-apache-web-server-on-the-instances-using-893eb2bbbcb3
